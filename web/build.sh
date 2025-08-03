c5ae91b320728c12b38b35269e21ca7e6017144d
Julia Ryan <juliaryan3.14@gmail.com>
2025-08-03 01:48:35 -0700

    Webpage skeleton

---
+++ web/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+dir=$(dirname "$0")
+
+wasm-pack build --target web --release "$dir"
+
+# TODO: entr, concat separate js file and copy assets into clean dir
+echo -e "now run:\n\n    static-web-server -d $dir -p 8080\n"
+echo "...and then open http://localhost:8080 in a browser"
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,43 +1,35 @@
 use std::array;
 
-use image::DynamicImage;
+use image::{DynamicImage, ImageFormat};
 use wasm_bindgen::{Clamped, prelude::*};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{ImageData, js_sys::Uint8Array};
+use web_sys::{Blob, ImageData, Response, js_sys::Uint8Array};
 
 // TODO: maybe install a panic hook
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn hiiii() {
-    alert("Hello, my-project!");
-}
-
-// TODO: pre-bake modifications like ghost piece?
 /// permanent, garbage, z, l, o, s, i, j, t
 type Skin = [DynamicImage; 9];
 
 #[wasm_bindgen]
-pub async fn fetch_url_binary(url: String) -> Result<Uint8Array, JsValue> {
+pub async fn fetch_url(url: String) -> Result<Blob, JsValue> {
     let window = web_sys::window().unwrap();
-    let promise = JsFuture::from(window.fetch_with_str(&url));
-    let result = promise.await?;
-    let response: web_sys::Response = result.dyn_into().unwrap();
-    let image_data = JsFuture::from(response.array_buffer()?).await?;
-    Ok(Uint8Array::new(&image_data))
+    let result = JsFuture::from(window.fetch_with_str(&url)).await?;
+    let response: Response = result.dyn_into().unwrap();
+    JsFuture::from(response.blob()?).await?.dyn_into()
 }
 
 // https://konsola5.github.io/jstris-customization-database/
 // https://i.imgur.com/HkJWOEQ.png
-async fn load_skin(url: String) -> Result<Skin, JsValue> {
-    let binary = fetch_url_binary(url).await?;
-    let altbuf = binary.to_vec();
+// TODO: maybe remove `image` dep and use js apis to reduce bundle size? check bloaty
+pub async fn load_skin(url: String) -> Result<Skin, JsValue> {
+    let blob = fetch_url(url).await?;
+    let mime = blob.type_();
+    let image_data = Uint8Array::new(&JsFuture::from(blob.array_buffer()).await?);
+    use ImageFormat::*;
+    let formats = [("png", Png), ("webp", WebP), ("qoi", Qoi), ("gif", Gif)];
+    let format = formats.iter().find_map(|(s, f)| mime.contains(s).then_some(*f)).unwrap_or(Png);
 
-    let mut image = image::load_from_memory_with_format(&altbuf, image::ImageFormat::Png).unwrap();
+    let mut image = image::load_from_memory_with_format(&image_data.to_vec(), format).unwrap();
     assert_eq!(
         image.width(),
         image.height() * 9,
@@ -50,31 +42,12 @@ async fn load_skin(url: String) -> Result<Skin, JsValue> {
 }
 
 #[wasm_bindgen]
-pub async fn show_image(url: String, canvas: String) -> Result<(), JsValue> {
-    let binary = fetch_url_binary(url).await?;
-    let altbuf = binary.to_vec();
-
-    let image = image::load_from_memory_with_format(&altbuf, image::ImageFormat::Png).unwrap();
-    let rgba_image = image.to_rgba8();
-    let clamped_buf: Clamped<&[u8]> = Clamped(rgba_image.as_raw());
-
-    // for (_, _, pixel) in rgba_image.enumerate_pixels_mut() {
-    //     if pixel[0] > 0 {
-    //         *pixel = image::Rgba([0, pixel[1], pixel[2], pixel[3]]);
-    //     }
-    // }
-
-    let image_data =
-        ImageData::new_with_u8_clamped_array_and_sh(clamped_buf, image.width(), image.height())?;
-
+pub async fn main() -> Result<(), JsValue> {
     let window = web_sys::window().unwrap();
     let document = window.document().expect("Could not get document");
     let canvas =
-        document.get_element_by_id(&canvas).unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
+        document.get_element_by_id("board").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
     let context =
         canvas.get_context("2d")?.unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>()?;
-
-    context.put_image_data(&image_data, 0.0, 0.0)?;
-
     Ok(())
 }
