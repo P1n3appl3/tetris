2d501d305ac7b3f0300d0cd8be0ee7748b8078ef
Julia Ryan <juliaryan3.14@gmail.com>
2025-08-03 02:21:19 -0700

    Set request_animation_frame handler

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,9 +1,10 @@
 use std::array;
 
 use image::{DynamicImage, ImageFormat};
+use tetris::{Config, Game};
 use wasm_bindgen::{Clamped, prelude::*};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Blob, ImageData, Response, js_sys::Uint8Array};
+use web_sys::{Blob, ImageData, Response, console, js_sys::Uint8Array};
 
 // TODO: maybe install a panic hook
 
@@ -11,7 +12,7 @@ use web_sys::{Blob, ImageData, Response, js_sys::Uint8Array};
 type Skin = [DynamicImage; 9];
 
 #[wasm_bindgen]
-pub async fn fetch_url(url: String) -> Result<Blob, JsValue> {
+pub async fn fetch_url(url: &str) -> Result<Blob, JsValue> {
     let window = web_sys::window().unwrap();
     let result = JsFuture::from(window.fetch_with_str(&url)).await?;
     let response: Response = result.dyn_into().unwrap();
@@ -21,7 +22,7 @@ pub async fn fetch_url(url: String) -> Result<Blob, JsValue> {
 // https://konsola5.github.io/jstris-customization-database/
 // https://i.imgur.com/HkJWOEQ.png
 // TODO: maybe remove `image` dep and use js apis to reduce bundle size? check bloaty
-pub async fn load_skin(url: String) -> Result<Skin, JsValue> {
+pub async fn load_skin(url: &str) -> Result<Skin, JsValue> {
     let blob = fetch_url(url).await?;
     let mime = blob.type_();
     let image_data = Uint8Array::new(&JsFuture::from(blob.array_buffer()).await?);
@@ -43,11 +44,39 @@ pub async fn load_skin(url: String) -> Result<Skin, JsValue> {
 
 #[wasm_bindgen]
 pub async fn main() -> Result<(), JsValue> {
+    console::log_1(&"initializing".into());
+    let window = web_sys::window().unwrap();
+    let document = window.document().expect("Could not get document");
+    let default_skin = "https://i.imgur.com/zjItrsg.png";
+    let skin = load_skin(default_skin);
+    console::time();
+    let func = Closure::<dyn FnMut()>::new(|| -> () {
+        console::time_log();
+        console::log_1(&"testing".into());
+        ()
+    });
+    window.request_animation_frame(func.as_ref().unchecked_ref())?;
+    let config = Config {
+        das: 6,
+        arr: 0,
+        gravity: 60,
+        soft_drop: 2,
+        lock_delay: (60, 300, 1200),
+        ghost: true,
+    };
+
+    let game = Game::new(config);
+    console::log_1(&"starting event loop".into());
+    Ok(())
+}
+
+pub fn draw(game: Game, skin: &Skin) -> Result<(), JsValue> {
     let window = web_sys::window().unwrap();
     let document = window.document().expect("Could not get document");
     let canvas =
         document.get_element_by_id("board").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
     let context =
         canvas.get_context("2d")?.unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    context.draw_image_with_image_bitmap(todo!(), 0.0, 0.0);
     Ok(())
 }
