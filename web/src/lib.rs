143e3166b0307bb422efa6f7126d1ec775e5044e
Julia Ryan <juliaryan3.14@gmail.com>
2025-08-04 23:27:45 -0700

    Draw some colors

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,13 +1,11 @@
 mod fps;
 mod skin;
 
-use std::array;
-
 use log::info;
 use tetris::{Config, Game};
-use wasm_bindgen::{Clamped, prelude::*};
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{Blob, ImageData, Response, console, js_sys::Uint8Array};
+use ultraviolet::DVec3;
+use wasm_bindgen::prelude::*;
+use web_time::Instant;
 
 #[wasm_bindgen]
 pub async fn main() -> Result<(), JsValue> {
@@ -18,8 +16,13 @@ pub async fn main() -> Result<(), JsValue> {
     let document = window.document().expect("Could not get document");
     let default_skin = "https://i.imgur.com/zjItrsg.png";
     let skin = skin::load_skin(default_skin);
-    let canvas =
+    let board =
         document.get_element_by_id("board").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let hold =
+        document.get_element_by_id("hold").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let ctx = hold.get_context("2d")?.unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let queue =
+        document.get_element_by_id("queue").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
     let timer_div =
         document.get_element_by_id("timer").unwrap().dyn_into::<web_sys::HtmlDivElement>()?;
     let fps_div =
@@ -37,11 +40,26 @@ pub async fn main() -> Result<(), JsValue> {
     let mut fps = fps::FPSCounter::new();
     let game = Game::new(config);
     info!("starting event loop");
+    let start_time = Instant::now();
     loop {
         raf_loop.next().await;
         let fps = fps.tick();
+        let t = (Instant::now() - start_time).as_secs_f64();
+        timer_div.set_text_content(Some(&format!("{t:.2}")));
         fps_div.set_text_content(Some(&format!("fps: {fps}")));
+        let (r, g, b) = fun_color(t / 10.0).into();
+        ctx.set_fill_style_str(&format!("rgb({r}, {g}, {b})"));
+        ctx.fill_rect(0.0, 0.0, hold.width() as f64, hold.height() as f64);
     }
+}
+
+// ty inigo <3
+pub fn fun_color(t: f64) -> DVec3 {
+    let a = DVec3::new(0.5, 0.5, 0.5);
+    let b = DVec3::new(0.5, 0.5, 0.5);
+    let c = DVec3::new(1.0, 1.0, 1.0);
+    let d = DVec3::new(0.0, 0.33, 0.67);
+    a + b * (6.283185 * (c * t + d)).map(|f| f.cos()) * 256.0
 }
 
 pub fn draw(game: Game, skin: &skin::Skin) -> Result<(), JsValue> {
